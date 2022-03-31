e78c49138e0049b2b276ce301222236cf612b2d7
sars723 <sarasalomonn@gmail.com>
2022-03-31 13:29:31 +0200

    airbnb-clone

---
+++ airbnb/components/Footer.js
@@ -0,0 +1,41 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p> Airbnb Plus</p>
+        <p> Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Hosting</h5>
+        <p>Try hosting</p>
+        <p>Visit our community forum</p>
+        <p>How to host responsibly</p>
+        <p> Explore hosting resources</p>
+        <p> AirCover: protection for Hosts</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>Airbnb.org: disaster relief housing</p>
+        <p>Combating discrimination</p>
+        <p>Support Afghan refugees</p>
+        <p> Celebrating diversity & belonging</p>
+        <p> Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p>Help Center</p>
+        <p>Our COVID-19 Response</p>
+        <p>Safety information</p>
+        <p> Supporting people with disabilities</p>
+        <p> Cancellation options</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
